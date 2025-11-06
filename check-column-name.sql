7a4efea0c131ecb9b182d059bbb7f346441bbd6f
Nguyễn Minh <nguyenduckminh0810@gmail.com>
2025-11-06 23:35:17 +0700

    Delete check-column-name.sql

--- check-column-name.sql
+++
@@ -1,8 +0,0 @@
--- Kiểm tra tên cột trong bảng khach_hang
-DESCRIBE auro1.khach_hang;
-
--- hoặc
-SHOW COLUMNS FROM auro1.khach_hang;
-
--- Xem data thực tế
-SELECT * FROM auro1.khach_hang WHERE id = 8;
